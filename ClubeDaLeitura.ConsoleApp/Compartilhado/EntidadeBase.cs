1567a02d1f92fc343c7f243ed1ec87fa69a0c5f5
Rafaela dos Santos Morais <morais.rafa508@gmail.com>
2024-05-07 16:06:14 -0300

    criação modulo compartilhado

---
+++ ClubeDaLeitura.ConsoleApp/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+namespace ClubeDaLeitura.ConsoleApp.Compartilhado
+{
+    internal abstract class EntidadeBase
+    {
+        public int Id { get; set; }
+        public abstract ArrayList Validar();
+
+        public abstract void AtualizarRegistro(EntidadeBase novoregistro);
+
+    }
+}
---
+++ ClubeDaLeitura.ConsoleApp/Compartilhado/TelaPrincipal.cs
@@ -0,0 +1,32 @@
+﻿namespace ClubeDaLeitura.ConsoleApp.Compartilhado
+{
+    internal static class TelaPrincipal
+    {
+        public static char ApresentarMenuPrincipal()
+        {
+            Console.Clear();
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("|           Clube da Leitura           |");
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("1 - Cadastro de Amigo");
+            Console.WriteLine("2 - Cadastro de Caixa");
+            Console.WriteLine("3 - Cadastro de Revista");
+            Console.WriteLine("4 - Cadastro de Empréstimo");
+            Console.WriteLine("5 - Cadastro de Reserva");
+            Console.WriteLine();
+            Console.WriteLine("S - Sair");
+
+            Console.WriteLine();
+
+            Console.Write("Escolha uma das opções: ");
+
+            char opcaoEscolhida = Console.ReadLine()[0];
+
+            return opcaoEscolhida;
+        }
+    }
+}
